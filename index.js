4485fca15a476cb199fe217bcd64cc6b9ad23042
med-more <166568621+med-more@users.noreply.github.com>
2025-11-01 17:47:54 +0000

    add DOM references for popup, chosen name display, and next button

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ const nameInput = document .getElementById('nameInput');
 const addBtn = document.getElementById('addBtn');
 const chooseBtn = document.getElementById('chooseBtn');
 const nameList = document.getElementById('nameList');
+
+const popup = document.getElementById('popup');
+const chosenName = document.getElementById('choosenName');
+const nextBtn = document.getElementById('nextBtn');
