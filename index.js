b313fe41db9581c0bb0ea40f1d551f3b7c94e27f
med-more <166568621+med-more@users.noreply.github.com>
2025-11-01 17:36:12 +0000

    Add basic DOM element selections

---
+++ index.js
@@ -0,0 +1,7 @@
+let names = [];
+let chosen = null;
+
+const nameInput = document .getElementById('nameInput');
+const addBtn = document.getElementById('addBtn');
+const chooseBtn = document.getElementById('chooseBtn');
+const nameList = document.getElementById('nameList');
